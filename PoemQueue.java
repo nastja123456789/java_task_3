a6ee7537367bc95568f5f132095bd4a773a50554
nastja123456789 <60920907+nastja123456789@users.noreply.github.com>
2023-08-28 07:04:00 +0300

    Update PoemQueue.java

--- PoemQueue.java
+++ PoemQueue.java
@@ -1,9 +1,11 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 
 public class PoemQueue {
     public static void main(String[] args) {
@@ -21,6 +23,48 @@ public class PoemQueue {
         }
         System.out.println("Кол-во строк в очереди: "+queue.size());
         printQueueWithDelay(queue);
+        try {
+            createJarFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createJarFile() throws IOException {
+        File mainfestFile = new File("MANIFEST.MF");
+        mainfestFile.createNewFile();
+        try (FileWriter writer=new FileWriter(mainfestFile)) {
+            writer.write("Main-Class: JarCreator\n");
+        }
+        JarOutputStream jarOutputStream=new JarOutputStream(new FileOutputStream("output.jar"),
+                createManifest());
+//        addToJar(jarOutputStream, "JarCreator.class");
+        addToJar(jarOutputStream, "poem.txt");
+        jarOutputStream.close();
+
+    }
+
+    public static Manifest createManifest() throws IOException {
+        FileInputStream manifestInput=new FileInputStream("MANIFEST.MF");
+        Manifest manifest=new Manifest(manifestInput);
+        manifest.clear();
+        return manifest;
+    }
+
+    public static void addToJar(JarOutputStream jarOutputStream,
+                                String filePath) throws IOException {
+        File file=new File(filePath);
+        JarEntry entry=new JarEntry(file.getName());
+        jarOutputStream.putNextEntry(entry);
+
+        FileInputStream inputStream=new FileInputStream(file);
+        byte[] buffer=new byte[1024];
+        int bytesRead;
+        while ((bytesRead=inputStream.read(buffer))!=-1) {
+            jarOutputStream.write(buffer,0,bytesRead);
+        }
+        inputStream.close();
+        jarOutputStream.closeEntry();
     }
     private static void printQueueWithDelay(Queue<String> queue) {
         Random random=new Random();
